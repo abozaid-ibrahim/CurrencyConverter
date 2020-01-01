90fcab8c5285f2a2e35b104441e989f6ac33b3db
abuzeid <abuzeid7@gmail.com>
2020-01-01 13:41:29 +0200

    Add init view for the app

--- CurrencyConverter/AppDelegate.swift
+++ CurrencyConverter/AppDelegate.swift
@@ -11,13 +11,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-//http://data.fixer.io/api/latest?access_key=753bfb556035f3badde59b452263f708&format=1&base=EUR
+    // http://data.fixer.io/api/latest?access_key=753bfb556035f3badde59b452263f708&format=1&base=EUR
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-         window = UIWindow(frame: UIScreen.main.bounds)
+        setInitView()
+        return true
+    }
+
+    private func setInitView() {
+        window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = CurrencySelectorViewController()
         window?.makeKeyAndVisible()
-        
-        return true
     }
 }
